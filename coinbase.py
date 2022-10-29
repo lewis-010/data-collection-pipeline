9172a21f49217edb4241f44fa6549c3beeb810a2
Lewis Cox <lcox101101@gmail.com>
2022-10-29 15:00:54 +0100

    create function to navigate to explore page

--- coinbase.py
+++ coinbase.py
@@ -1,5 +1,8 @@
 from selenium import webdriver
 import time
 
-driver = webdriver.Chrome()
-driver.get("https://www.coinbase.com/explore")
+# navigates to the explore page on coinbase, displaying the top 30 coins by market-cap
+def navigate_to_explore():
+    driver = webdriver.Chrome()
+    driver.get("https://www.coinbase.com")
+
