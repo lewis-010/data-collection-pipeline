015b558142c1d1f55adda8cbedc3f54e8c04bd57
Lewis Cox <lcox101101@gmail.com>
2022-10-29 15:13:24 +0100

    add webdriver manager to enable chromedriver

--- coinbase.py
+++ coinbase.py
@@ -1,8 +1,11 @@
 from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
 import time
 
 # navigates to the explore page on coinbase, displaying the top 30 coins by market-cap
 def navigate_to_explore():
-    driver = webdriver.Chrome()
+    driver = webdriver.Chrome(ChromeDriverManager().install())
     driver.get("https://www.coinbase.com")
+    time.sleep(10)
 
+navigate_to_explore()
