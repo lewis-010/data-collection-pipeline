a404b4ba2f7e8202516b7a1a89150d7bbaf96346
Lewis Cox <lcox101101@gmail.com>
2022-10-29 16:53:53 +0100

    add try code for accepting cookies

--- coinbase.py
+++ coinbase.py
@@ -1,15 +1,28 @@
+from lib2to3.pgen2 import driver
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
 from webdriver_manager.chrome import ChromeDriverManager
 import time
 
-# navigates to the explore page on coinbase, displaying the top 30 coins by market-cap
-def navigate_to_explore():
+def accept_cookies():
     driver = webdriver.Chrome(ChromeDriverManager().install())
     driver.get('https://www.coinbase.com')
     driver.maximize_window()
     time.sleep(3)
+    try:
+        WebDriverWait(driver,5).until(EC.presence_of_element_located((By.XPATH, "//*[@id='root']/div[2]/div[2]/button[2]")))
+    except TimeoutException:
+        print('Loading timed out.')
 
-    button = driver.find_element_by_id('header-price-link')
+# navigates to the explore page on coinbase, displaying the top 30 coins by market-cap
+def navigate_to_explore():
+
+    
+    button = driver.find_element(by=By.XPATH, value='//button[@class="Button-sc-111z0u6-0 iaBuWF"]')
     button.click()
+    time.sleep(3)
 
 navigate_to_explore()
