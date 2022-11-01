5f53af82f417d3ffb00318af21da846375859c53
Lewis Cox <lcox101101@gmail.com>
2022-11-01 16:55:30 +0000

    remove unused code

--- crypto.py
+++ crypto.py
@@ -10,10 +10,11 @@ import time
 class Scraper():
     def __init__(self):
         self.link_number = 0 # number of links that will be taken from the webpage
+        self.driver = webdriver.Chrome(ChromeDriverManager().install())    
 
     # accept cookies on crypto.com homepage
     def accept_cookies(self) -> webdriver.Chrome:
-        driver = webdriver.Chrome(ChromeDriverManager().install())
+        driver = self.driver
         driver.get('https://crypto.com/eea')
         driver.maximize_window()
         time.sleep(3)
@@ -23,10 +24,11 @@ class Scraper():
         except TimeoutException:
             print('Loading timed out.')
         
-        return driver
+        return self.driver
 
     # navigate to the price page that displays the top 50 coins by market cap
     def navigate_to_explore(self, driver: webdriver.Chrome):
+        driver = self.driver
         try:
             WebDriverWait(driver,5).until(EC.presence_of_element_located((By.XPATH, "//*[@id='gatsby-focus-wrapper']/main/div[1]/div/div/div/div[1]/a/button"))).click()
             time.sleep(3)
@@ -34,21 +36,23 @@ class Scraper():
         except TimeoutException:
             print('Loading timed out.')
         
-        return driver
+        return self.driver
         
 
     # skip the tour of the price page
     def skip_tour(self, driver: webdriver.Chrome):
+        driver = self.driver
         try:
             WebDriverWait(driver,5).until(EC.presence_of_element_located((By.XPATH, "//*[@id='__next']/div[3]/div[2]/div[2]/div[1]/div"))).click()
             time.sleep(3)
         except TimeoutException:
             print('Loading timed out.')
         
-        return driver
+        return self.driver
 
     # get links for the top 50 coins by market cap
     def get_links(self, driver: webdriver.Chrome) -> list:
+        driver = self.driver
         table = WebDriverWait(driver,10).until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, ".css-1v8x7dw [href]")))
         table = driver.find_elements(by=By.CSS_SELECTOR, value=".css-1v8x7dw [href]")
         links = [elem.get_attribute('href') for elem in table]
@@ -58,14 +62,6 @@ class Scraper():
         driver.quit()
 
         return(links)
-
-    # if __name__=="__main__":
-        driver = accept_cookies()
-        navigate_to_explore(driver) 
-        driver.switch_to.window(driver.window_handles[1]) # swtich to latest window
-        skip_tour(driver)
-        get_links(driver)
-        driver.quit()
 
 crypto = Scraper()
 crypto.accept_cookies()
