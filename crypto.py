8b29c3ec3f374883f73aa19ff658ad508953d3da
Lewis Cox <lcox101101@gmail.com>
2022-11-01 15:06:53 +0000

    add webdriverwait to get links function

--- crypto.py
+++ crypto.py
@@ -43,6 +43,8 @@ def skip_tour(driver: webdriver.Chrome):
 
 # get links for the top 50 coins by market cap
 def get_links(driver: webdriver.Chrome) -> list:
+    WebDriverWait(driver,5).until(EC.presence_of_element_located((By.XPATH, "//*[@id='__next']/div[3]/div[2]/div/div[3]/div[3]/table/tbody/tr[1]/td[3]/a")))
+    time.sleep(3)
     coin = driver.find_element(by=By.XPATH, value="//*[@id='__next']/div[3]/div[2]/div/div[3]/div[3]/table/tbody/tr[1]/td[3]/a")
     a_tag = coin.find_element(by=By.TAG_NAME, value='a')
     link = a_tag.get_attribute('href')
