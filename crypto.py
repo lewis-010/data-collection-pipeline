037590f4b6ff60acb15abf09c2441b3e892a24db
Lewis Cox <lcox101101@gmail.com>
2022-11-01 14:59:11 +0000

    add get_link function to get links for prices

--- crypto.py
+++ crypto.py
@@ -31,7 +31,26 @@ def navigate_to_explore(driver: webdriver.Chrome):
     
     return driver
 
+# skip the tour of the price page
+def skip_tour(driver: webdriver.Chrome):
+    try:
+        WebDriverWait(driver,5).until(EC.presence_of_element_located((By.XPATH, "//*[@id='__next']/div[3]/div[2]/div[2]/div[1]/div"))).click()
+        time.sleep(3)
+    except TimeoutException:
+        print('Loading timed out.')
+    
+    return driver
+
+# get links for the top 50 coins by market cap
+def get_links(driver: webdriver.Chrome) -> list:
+    coin = driver.find_element(by=By.XPATH, value="//*[@id='__next']/div[3]/div[2]/div/div[3]/div[3]/table/tbody/tr[1]/td[3]/a")
+    a_tag = coin.find_element(by=By.TAG_NAME, value='a')
+    link = a_tag.get_attribute('href')
+    print(link)
+
 driver = accept_cookies()
 navigate_to_explore(driver)
 window_after = driver.window_handles[1]
 driver.switch_to.window(window_after)
+skip_tour(driver)
+get_links(driver)
