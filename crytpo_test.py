b9fa8cdcef85e0bb8562d3865c477a6047048c0d
Lewis Cox <lcox101101@gmail.com>
2022-11-09 13:47:07 +0000

    import scraper class into test file

--- crytpo_test.py
+++ crytpo_test.py
@@ -3,14 +3,19 @@ from webdriver_manager.chrome import ChromeDriverManager
 import crypto
 import unittest
 
+Crypto = crypto.Scraper() # allows crytpo.py methods to be called and tested
+
 class TestCrypto(unittest.TestCase):
 
     def setUp(self):
-        self.driver = webdriver.Chrome(ChromeDriverManager().install())
+        self.driver = webdriver.Chrome(ChromeDriverManager().install())   
 
     def test_accept_cookies(self):
         driver = self.driver
-        
+        driver.get('https://crypto.com/eea')
+        driver.maximize_window()
+        self.assertAlmostEqual(Crypto.accept_cookies)
+
 
     def tearDown(self):
         self.driver.close()
