680114e95df4f043a2ad17d61b90e448a61545a6
Lewis Cox <lcox101101@gmail.com>
2022-11-09 13:34:23 +0000

    import selenium & chromedriver into test file

--- crytpo_test.py
+++ crytpo_test.py
@@ -1,10 +1,19 @@
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
 import crypto
 import unittest
 
 class TestCrypto(unittest.TestCase):
 
+    def setUp(self):
+        self.driver = webdriver.Chrome(ChromeDriverManager().install())
+
     def test_accept_cookies(self):
-        self.assertEqual(crypto)
+        driver = self.driver
+        
+
+    def tearDown(self):
+        self.driver.close()
 
 if __name__=='__main__':
     unittest.main()
