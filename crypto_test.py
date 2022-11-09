9760d5cfe99eb46edf7cc04419eed390904bb86f
Lewis Cox <lcox101101@gmail.com>
2022-11-09 16:50:58 +0000

    fix test_accept_cookies and add tests for others

--- crypto_test.py
+++ crypto_test.py
@@ -1,13 +1,13 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.common.exceptions import TimeoutException
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+# from selenium.common.exceptions import TimeoutException
+# from selenium.webdriver.support.ui import WebDriverWait
+# from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
 import unittest
 import crypto
 
-Crypto = crypto.Scraper() # allows crytpo.py methods to be called and tested
+Crypto = crypto.Scraper() # allows crypto.py methods to be called and tested
 
 class TestCrypto(unittest.TestCase):
 
@@ -16,13 +16,21 @@ class TestCrypto(unittest.TestCase):
 
     def test_accept_cookies(self):
         Crypto.accept_cookies()
-        try:
-            visibility = Crypto.driver.find_element((By.XPATH, "//*[@id='onetrust-banner-sdk']"))
-            print(visibility)
-        except TimeoutException:
-            return
-        raise Exception('Cookies pop-up has not disappeared')
-    
+        cookies_banner = Crypto.driver.find_element(by=By.XPATH, value = "//*[@id='onetrust-banner-sdk']")
+        style_attribute = cookies_banner.get_attribute("style")
+        self.assertIn("hidden", style_attribute)
+        print('The accept cookies button has been clicked.')      
+        
+    def test_navigate_to_prices(self):
+        Crypto.navigate_to_prices()
+
+    def test_skip_tour(self):
+        Crypto.skip_tour()
+        skip_tour_tab = Crypto.driver.find_element(by=By.XPATH, value = "//*[@id='__next']/div[3]/div[2]")
+        self.assertNotIn(skip_tour_tab)
+        print('The skip tour button has been clicked.')
+
+
     def tearDown(self):
         self.driver.quit()
 
