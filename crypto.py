b7fc78cf8f92ed292d0f8b713391fd1bc0efc924
Lewis Cox <lcox101101@gmail.com>
2022-11-13 15:29:30 +0000

    move import options to within init method

--- crypto.py
+++ crypto.py
@@ -1,6 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.options import Options
+# from selenium.webdriver.chrome.options import Options
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -31,7 +31,7 @@ class Scraper():
         self.links = []
         self.dict_data = {}
         self.data_list = []
-        options = Options()
+        options = webdriver.ChromeOptions()
         options.add_argument("--headless")
         options.add_argument("window-size=1920,1080")
         self.driver = webdriver.Chrome(ChromeDriverManager().install(), chrome_options = options)
